66ee08db4327a9b510500a2a48b23d460c0747e8
Arto K <arto@d1stkfactory>
2015-02-20 02:07:41 +0200

    Initial push

---
+++ scripts.js
@@ -0,0 +1,16 @@
+var wedApp = angular.module('wedApp', ['ngRoute']);
+
+wedApp.controller('MainCtrl' ,function($scope) {
+	$scope.message = 'yet';
+	}); 
+
+wedApp.config(['$routeProvider',
+				function($routeProvider) {
+					$routeProvider.when('/juhlat', {
+						templateUrl: 'partials/juhlat.html',
+						controller: 'mainCtrl'
+					}).when('/info', {
+						templateUrl: 'partials/info.html',
+						controller: 'mainCtrl'})
+					}]);
+
