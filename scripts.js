7cb8103fb282ae1c21f5678c425a39914d25a27a
Arto K <arto@d1stkfactory>
2015-02-21 23:56:43 +0200

    Some trivial controller functionality

--- scripts.js
+++ scripts.js
@@ -2,18 +2,27 @@ var wedApp = angular.module('wedApp', ['ngRoute']);
 
 wedApp.controller('MainCtrl' ,function($scope) {
 	$scope.message = 'yet';
-	}); 
+});
+
+wedApp.controller('LoginCtrl', function($scope, $window) {
+	$scope.alert = function(login) {
+			var name = login.name;
+			var pwd = login.pwd;
+			login.name = "";
+			login.pwd = "";
+			$window.alert('Kirjoitit '+name+' '+pwd);
+}});
 
 wedApp.config(['$routeProvider',
 				function($routeProvider) {
 					$routeProvider.when('/juhlat', {
 						templateUrl: 'partials/juhlat.html',
-						controller: 'mainCtrl'
+						controller: 'MainCtrl'
 					}).when('/info', {
 						templateUrl: 'partials/info.html',
-						controller: 'mainCtrl'
+						controller: 'MainCtrl'
 					}).otherwise({
 						templateUrl: 'partials/main.html',
-						controller: 'mainCtrl'
-					})}]);
+						controller: 'MainCtrl'
+})}]);
 
